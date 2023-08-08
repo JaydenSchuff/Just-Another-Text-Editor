ffb9073af273b34633657ab8cd33441395e1543e
Jayden Schuffenhauer <jdrich135@gmail.com>
2023-08-08 14:12:38 -0600

    completed src-sw.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,39 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
+       // HtmlWebpackPlugin for generating HTML files
+       new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['main'], // Which entry point to include
+      }),
 
+      // WebpackPwaManifest for generating a manifest.json file
+      new WebpackPwaManifest({
+        name: 'My App',
+        short_name: 'App',
+        description: 'My Progressive Web App',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512], // Icon sizes
+          },
+        ],
+      }),
+
+      // Workbox InjectManifest plugin for service worker
+      new InjectManifest({
+        swSrc: './src/sw.js', // Path to your service worker script
+      }),
+    ],
     module: {
       rules: [
-        
+        // CSS loader rule
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
       ],
     },
   };
